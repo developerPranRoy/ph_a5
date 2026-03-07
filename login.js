1fb4815f7a276decb68f7a4cd6e76471cd200713
Pran Roy <pranroy310@gmail.com>
2026-03-07 06:44:30 +0600

    login page

---
+++ login.js
@@ -0,0 +1,12 @@
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", () => {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (username === "admin" && password === "admin123") {
+        window.location.href = "index.html";
+    } else {
+        alert("Invalid credentials");
+    }
+});
